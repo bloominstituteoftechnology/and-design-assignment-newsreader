1e1a2c116b6d08a41c76261ce7048eab91b5f82d
stilljack <stilljack@gmail.comconfig --global user.email stilljack@gmail.com>
2019-08-14 12:35:23 -0700

    a
    and

---
+++ app/src/main/java/com/example/and_design_assignment_newsreader/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.and_design_assignment_newsreader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
