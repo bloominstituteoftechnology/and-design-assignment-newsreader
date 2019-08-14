f25650794a090fbc6c4476d9de2b2d0f812882ab
Albert Yakubov <mrayakubov@hotmail.com>
2019-08-14 15:43:17 -0600

    Scroll View
    Build simple list in scroll view

---
+++ app/src/main/java/com/example/redingnewsmaterialui/List.kt
@@ -0,0 +1,12 @@
+package com.example.redingnewsmaterialui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class List : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
