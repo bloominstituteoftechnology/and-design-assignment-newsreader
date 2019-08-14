35bb8173fbc3a49e49890a64052296fdcfc83a5e
Albert Yakubov <mrayakubov@hotmail.com>
2019-08-14 16:27:41 -0600

    Material UI
    add all code to pass between activities and all the necessary components for UI

--- app/src/main/java/com/example/redingnewsmaterialui/List.kt
+++ app/src/main/java/com/example/redingnewsmaterialui/List.kt
@@ -1,12 +1,29 @@
 package com.example.redingnewsmaterialui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_list.*
+import kotlinx.android.synthetic.main.image_view_layout.*
+import kotlinx.android.synthetic.main.image_view_layout.image_view
 
 class List : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+        image_view.setOnClickListener {
+            val intent = Intent(this, Detail::class.java)
+            startActivity(intent)
+        }
+        image_view1.setOnClickListener {
+            val intent = Intent(this, Detail::class.java)
+            startActivity(intent)
+        }
+        image_view2.setOnClickListener {
+            val intent = Intent(this, Detail::class.java)
+            startActivity(intent)
+        }
     }
 }
