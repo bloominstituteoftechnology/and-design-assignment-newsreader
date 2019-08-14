c8eb452d387af2c1486c95336c4414583cd31e0a
stilljack <stilljack@gmail.comconfig --global user.email stilljack@gmail.com>
2019-08-14 15:16:33 -0700

    learning a lot of super cool tools, found the use of guidelines and how you can restrict the size and even shape of images in very granular ways

---
+++ app/src/main/java/com/example/newsreader/acitivties/DetailsAcitivity.kt
@@ -0,0 +1,13 @@
+package com.example.newsreader.acitivties
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.newsreader.R
+
+class DetailsAcitivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details_acitivity)
+    }
+}
